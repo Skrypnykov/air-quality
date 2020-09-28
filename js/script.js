babb54f2fdee7d011bb990305bdf5ffc53c59c33
Oleh Skrypnykov <oskripnikov@gmail.com>
2020-09-28 23:51:35 +0300

    add media request

--- js/script.js
+++ js/script.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
         items:3,
         nav:true,
         dots:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:2,
+                nav:false
+            },
+            1000:{
+                items:3,
+                nav:true,
+            }
+        }
     });
 });
 
